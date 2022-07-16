fbc568942b6358716eb7a9dec9e8e978ca27dd0d
wetlo <genericgithub@tutanota.com>
2022-07-16 11:20:32 +0200

    implement image viewing

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,61 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
 
+use std::{error::Error, fs::File, io::BufReader, io::Read, path::Path};
+
+use tauri::{
+    http::{self, ResponseBuilder},
+    AppHandle, Runtime,
+};
+use url::Url;
+
+fn image_format<P: AsRef<Path>>(path: P) -> Option<&'static str> {
+    let accepted_formats = ["png", "jpg", "jpeg"];
+
+    path.as_ref()
+        .extension()
+        .and_then(|ext| {
+            accepted_formats
+                .iter()
+                .find(|i| ext.to_string_lossy() == **i)
+        })
+        .copied()
+}
+
+fn get_image<R: Runtime>(
+    _app: &AppHandle<R>,
+    req: &http::Request,
+) -> Result<http::Response, Box<dyn Error>> {
+    let uri = Url::parse(dbg!(req.uri()))?;
+
+    let path = uri.path();
+
+    let res = if let Some(i) = image_format(path) {
+        let f = File::open(path)?;
+        let mut r = BufReader::new(f);
+        let mut content = Vec::new();
+
+        r.read_to_end(&mut content)?;
+
+        ResponseBuilder::new()
+            .status(200)
+            .mimetype(&format!("image/{}", i))
+            .body(content)
+    } else {
+        ResponseBuilder::new()
+            .status(400)
+            .body("invalid file type".into())
+    }?;
+
+    Ok(res)
+}
+
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    dbg!("Hello world");
+    tauri::Builder::default()
+        .register_uri_scheme_protocol("image", get_image)
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,33 @@
 <script lang="ts">
+	import { open } from "@tauri-apps/api/dialog";
 	export let name: string;
+
+	let imgProm: Promise<string> = new Promise(() => {
+		while (true) {} // halt the promise
+	});
+
+	let choose = () =>
+		(imgProm = open({
+			multiple: false,
+			filters: [
+				{
+					name: "Image",
+					extensions: ["png", "jpg"],
+				},
+			],
+		})
+			.then((s) => (Array.isArray(s) ? s[0] : s))
+			.then((s) => "image://localhost" + s));
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
+	{#await imgProm}
+		<button on:click={choose}> Choose image </button>
+	{:then img}
+		<p>{img}</p>
+		<img src={img} alt="System" />
+	{/await}
 </main>
 
 <style>
