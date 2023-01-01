fbf1eeff6ac18366a3de38091b7f225880150436
wetlo <genericgithub@tutanota.com>
2023-01-01 22:04:11 +0100

    add basic layout of library

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,27 @@
+<script>
+    import { Divider } from "attractions";
+    import { MenuIcon } from "svelte-feather-icons";
+</script>
+
+<div class="flex row">
+    <div class="navbar">
+        <h1 class="flex row v-center"><MenuIcon />ComicLazer</h1>
+        <Divider />
+        <h2>Settings</h2>
+    </div>
+    <div class="main-content">
+        <slot />
+    </div>
+</div>
+
+<style>
+    .navbar {
+        position: static;
+        width: 20vw;
+        height: 100vh;
+    }
+
+    .main-content {
+        width: 100%;
+    }
+</style>
--- src/routes/library.svelte
+++ src/routes/library.svelte
@@ -1,17 +1,67 @@
 <script lang="ts">
     import { invoke } from "@tauri-apps/api/tauri";
     import { each } from "svelte/internal";
+    import Navbar from "../components/Navbar.svelte";
     import type { Comic } from "../entities/Comic";
 
-    let comics = invoke("all_comics").then((v) => v as Comic[]);
+    async function get_comics() {
+        const cs = (await invoke("all_comics")) as Comic[];
+
+        for (let i = 0; i < cs.length; i++) {
+            const c = cs[i];
+            c.chapters = [
+                await invoke("chapter", { comicId: c.id, chapterNumber: 1 }),
+            ];
+        }
+
+        console.log(cs);
+        return cs;
+    }
 </script>
 
-{#await comics}
-    loading comics
-{:then cs}
-    <ul>
-        {#each cs as c}
-            <li><a href="#/detail/{c.id}">{c.name}</a></li>
-        {/each}
-    </ul>
-{/await}
+<Navbar>
+    {#await get_comics()}
+        loading comics
+    {:then cs}
+        <div class="flex space-between v-center comics">
+            {#each cs as c}
+                <a href="#/detail/{c.id}" class="card">
+                    <img
+                        alt="{c.name} cover"
+                        src="comic://localhost{c.chapters[0].path}?page=1"
+                    />
+                    <div class="info">
+                        <p>{c.name}</p>
+                    </div>
+                </a>
+            {/each}
+        </div>
+    {/await}
+</Navbar>
+
+<style>
+    .card {
+        position: relative;
+        width: 8vw;
+    }
+
+    .info {
+        width: 100%;
+        background-color: rgba(0, 0, 0, 0.4);
+        position: absolute;
+        bottom: 0;
+    }
+
+    .comics {
+        flex-wrap: wrap;
+    }
+
+    img {
+        width: 100%;
+    }
+
+    p {
+        text-align: start;
+        color: white;
+    }
+</style>
